b22e2c0ee50d3bb9df08a1bf04e688ccac69847a
patsicko <patsicko@g>
2022-08-31 12:41:00 +0200

    Form with formik

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Users from './components/users';
+import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Users/>
+    </div>
+  );
+}
+
+export default App;
